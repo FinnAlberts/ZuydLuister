7e6280ca3f1191ba2966cbb4d1daca988ab3275f
Finn Alberts <2062662alberts@zuyd.nl>
2021-01-14 13:25:08 +0100

    App.xaml.cs aangepast zodat alles binnen een NavigationPage werkt. Ook de MainPage verwijdert en de pagina om te openen veranderd naar LoginPage.

--- ZuydLuister/ZuydLuister/App.xaml.cs
+++ ZuydLuister/ZuydLuister/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ZuydLuister
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()

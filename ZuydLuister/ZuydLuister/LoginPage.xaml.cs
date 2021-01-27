d124fa24c85f57281df608a579ceea15bfcd0db0
Finn Alberts <2062662alberts@zuyd.nl>
2021-01-27 16:29:24 +0100

    - Menu depends on permissions

--- ZuydLuister/ZuydLuister/LoginPage.xaml.cs
+++ ZuydLuister/ZuydLuister/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZuydLuister.Model;
 
 namespace ZuydLuister
 {
@@ -30,6 +32,34 @@ namespace ZuydLuister
             }
             else
             {
+                using (SQLiteConnection connection = new SQLiteConnection(App.GameDatabaseLocation))
+                {
+                    connection.CreateTable<Administrator>();
+                    var administrators = connection.Table<Administrator>().ToList();
+                    administrators = (from administrator in administrators where usernameEntry.Text == administrator.AdminEmail select administrator).ToList();
+
+                    if (administrators.Count > 0)
+                    {
+                        if (administrators[0].IsMasterAdmin)
+                        {
+                            permissions = 2;
+                        }
+                        else
+                        {
+                            permissions = 1;
+                        }
+                    } 
+                    else
+                    {
+                        permissions = 0;
+                    }
+
+                    if (usernameEntry.Text.ToLower() == "luister@zuyd.nl")
+                    {
+                        permissions = 2;
+                    }
+                }
+                
                 Navigation.PushAsync(new MenuPage());
             }
         }
